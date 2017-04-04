5b70fa5c6159a610add205085d6ded0928547f09
Sebastian Skałacki <skalee@gmail.com>
2017-04-04 03:14:41 +0200

    When loading Iliad, reject very short paragraphs

--- lib/well_read_faker/load_bundled_sources.rb
+++ lib/well_read_faker/load_bundled_sources.rb
@@ -3,6 +3,7 @@ WellReadFaker.add_source(
   File.expand_path("../../books/homer_butler_iliad.txt", __FILE__),
   begin: /The quarrel between Agamemnon/,
   end: /End of the Project Gutenberg EBook of The Iliad, by Homer/,
+  min_words: 5,
 )
 
 WellReadFaker.default_source = :iliad
